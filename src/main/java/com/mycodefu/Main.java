41ea8d71da0982c29af531ae8c30ecb8fb9acb37
Luke Thompson <endeavour9@gmail.com>
2018-09-22 17:59:39 +1000

    add javafx window

--- src/main/java/com/mycodefu/Main.java
+++ src/main/java/com/mycodefu/Main.java
@@ -1,7 +1,25 @@
 package com.mycodefu;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hi");
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+        Group mainNode = createMainNode(primaryStage);
+        Scene scene = createScene(mainNode);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    Scene createScene(Group mainNode) {
+        return new Scene(mainNode, 1024, 768, Color.BLACK);
+    }
+
+    Group createMainNode(Stage primaryStage) {
+        return new Group();
     }
 }
