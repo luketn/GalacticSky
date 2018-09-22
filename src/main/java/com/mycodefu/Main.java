20f539ea75a54e9ccdbe2151aef54a77024b8686
Luke Thompson <endeavour9@gmail.com>
2018-09-22 18:28:56 +1000

    add a background image: https://www.maxpixel.net/Cosmos-Universe-Galaxy-Space-Tree-Background-1721695

--- src/main/java/com/mycodefu/Main.java
+++ src/main/java/com/mycodefu/Main.java
@@ -3,7 +3,9 @@ package com.mycodefu;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,7 +18,7 @@ public class Main extends Application {
     }
 
     Scene createScene(Group mainNode) {
-        return new Scene(mainNode, 1024, 768, Color.BLACK);
+        return new Scene(mainNode, 1920,1080, new ImagePattern(new Image(this.getClass().getResourceAsStream("/background.jpg"))));
     }
 
     Group createMainNode(Stage primaryStage) {
