49ca105b861a30297131d60a6ca1a396ec951cb8
Luke Thompson <endeavour9@gmail.com>
2018-09-22 19:02:06 +1000

    Add a spaceship and keystroke arrow movement

--- src/main/java/com/mycodefu/Main.java
+++ src/main/java/com/mycodefu/Main.java
@@ -2,19 +2,48 @@ package com.mycodefu;
 
 import javafx.application.Application;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.ImagePattern;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    public static final int SPEED = 15;
+    private ImageView spaceship;
+
     public void start(Stage primaryStage) throws Exception {
         Group mainNode = createMainNode(primaryStage);
         Scene scene = createScene(mainNode);
+        addKeyListeners(scene);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private void addKeyListeners(Scene scene) {
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            switch (event.getCode()) {
+                case DOWN: {
+                    spaceship.setY(spaceship.getY()+ SPEED);
+                    break;
+                }
+                case UP: {
+                    spaceship.setY(spaceship.getY()-SPEED);
+                    break;
+                }
+                case LEFT: {
+                    spaceship.setX(spaceship.getX()-SPEED);
+                    break;
+                }
+                case RIGHT: {
+                    spaceship.setX(spaceship.getX()+SPEED);
+                    break;
+                }
+            }
+        });
     }
 
     Scene createScene(Group mainNode) {
@@ -22,6 +51,11 @@ public class Main extends Application {
     }
 
     Group createMainNode(Stage primaryStage) {
-        return new Group();
+        return new Group(createSpaceShip());
+    }
+
+    private Node createSpaceShip() {
+        this.spaceship = new ImageView(new Image(getClass().getResourceAsStream("/spaceship.png")));
+        return spaceship;
     }
 }
